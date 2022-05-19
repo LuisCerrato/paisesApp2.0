8b4884405ae320838706fc839109263e050e0916
Luis Cerrato <luiscerrato9@gmail.com>
2022-05-18 23:03:23 -0600

    sidebar y avance

--- src/app/Shared/shared.module.ts
+++ src/app/Shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -10,9 +11,11 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
   exports:[
     SidebarComponent
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
